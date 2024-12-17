b65097e4177a69a3d0c0bfd280e4a433ed520019
Omarespo-dev <omaresposito21@gmail.com>
2024-12-17 18:58:56 +0100

    finish exercise

--- js/script.js
+++ js/script.js
@@ -16,23 +16,36 @@
 //-- Calcolare il prezzo totale del viaggio --
 
     //1 Definire il prezzo del biglietto in base ai km (0.21 € al km)
-
+        
+        let prezzoAlkm = 0.21;
+        let prezzoBase = km*prezzoAlkm;
+        
+        
 
 
     // 2 Applicare uno sconto del 20% per i minorenni
-
-
+        let prezzoSconto = 0;
+        if (età < 18) {
+            prezzoSconto = prezzoBase * 0.20; // Sottrai il 20% dal totale
+        }
 
     // 3 applicare uno sconto del 40% per gli over 65
+        
+        else if (età > 65) {
+            prezzoSconto = prezzoBase * 0.40; // Sottrai il 40% dal totale
+        }
 
 
+//      
 
-// 
+    // // Prezzo intero per chi non usufruisce lo sconto
 
 
 
 // OUTPUT PREZZO FINALE
+        let prezzoFinale = prezzoBase - prezzoSconto;
 
+        console.log ("Il prezzo finale sarà:" + prezzoFinale.toFixed(2) + "€")
 
 
 
